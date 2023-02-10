56e034991cd949cfa57ad4f25b9c128b912043ae
seunoyeniyi <iamseunoyeniyi@gmail.com>
2023-02-10 15:29:27 +0100

    commit

--- server.js
+++ server.js
@@ -58,3 +58,6 @@ const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Node.js listening on port " + listener.address().port);
 });
 
+
+// Export the Express API
+module.exports = app
